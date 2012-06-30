53e282eb9b7e796ef24b6ad85108915dae929511
goatherd <maik@phpkuh.de>
2012-06-30 16:13:40 +0200

    Replaced in_array with faster isset(). PEAR standards.

--- Sniffs/PHP/ForbiddenParameterSniff.php
+++ Sniffs/PHP/ForbiddenParameterSniff.php
@@ -5,98 +5,105 @@
  *
  * PHP version 5
  *
- * @category PHP
- * @package PHP_CodeSniffer
- * @author Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
  * @copyright 2012 foobugs oelke & eichner GbR
- * @license BSD http://www.opensource.org/licenses/bsd-license.php
- * @link https://github.com/foobugs/PHP53to54
- * @since 1.0-beta
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
  */
 
 /**
  * Forbidden Function Parameter
- * 
+ *
  * Search for function or closure definitions that use global variable as their
  * parameters.
  *
- * @category PHP
- * @package PHP_CodeSniffer
- * @author Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
  * @copyright 2012 foobugs oelke & eichner GbR
- * @license BSD http://www.opensource.org/licenses/bsd-license.php
- * @link https://github.com/foobugs/PHP53to54
- * @since 1.0-beta
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
  */
 class PHP53to54_Sniffs_PHP_ForbiddenParameterSniff
-	extends PHP53to54_AbstractSniff
-	implements PHP_CodeSniffer_Sniff
+extends PHP53to54_AbstractSniff
 {
-	/**
-	 * A list of tokenizers this sniff supports.
-	 *
-	 * @var array
-	 */
-	public $supportedTokenizers = array(
-		'PHP',
-	);
-	
-	/**
-	 * A list of removed functions with their parameters associated regular
-	 * expression that are not allowed anymore.
-	 * 
-	 * @var array(string => array(string, [string]))
-	 */
-	protected $forbiddenParameterNames = array(
-		'$GLOBALS',
-		'$_SERVER',
-		'$_GET',
-		'$_POST',
-		'$_SET',
-		'$_FILES',
-		'$_COOKIE',
-		'$_SESSION',
-		'$_REQUEST',
-		'$_ENV',
-	);
-	
-	/**
-     * Returns an array of tokens this test wants to listen for.
+    /**
+     * A list of tokenizers this sniff supports.
      *
-     * @return array
+     * @var array
      */
-	public function register()
-	{
-		return array(
-			T_FUNCTION,
-			T_CLOSURE,
-		);
-	}
-	
-	/**
+    public $supportedTokenizers = array(
+        'PHP',
+    );
+
+    /**
+     * A list of removed functions with their parameters associated regular
+     * expression that are not allowed anymore.
+     *
+     * @var array(string => array(string, [string]))
+     */
+    protected $forbiddenParameterNames = array(
+        '$GLOBALS' => true,
+        '$_SERVER' => true,
+        '$_GET' => true,
+        '$_POST' => true,
+        '$_SET' => true,
+        '$_FILES' => true,
+        '$_COOKIE' => true,
+        '$_SESSION' => true,
+        '$_REQUEST' => true,
+        '$_ENV' => true,
+    );
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     * @see PHP_CodeSniffer_Sniff::register()
+     */
+    public function register()
+    {
+        return array( T_FUNCTION, T_CLOSURE, );
+    }
+
+    /**
      * Processes this test, when one of its tokens is encountered.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
      * @param int                  $stackPtr  The position of the current token in
-     *                                        the stack passed in $tokens.
+     *                                         the stack passed in $tokens.
+     *
      * @return void
+     * @see PHP_CodeSniffer_Sniff::process()
      */
-	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-	{
-		$tokens = $phpcsFile->getTokens();
-		$functionNamePtr = $phpcsFile->findNext(array(T_STRING), ($stackPtr + 1), null, false);
-		
-		$parameterTokens = $this->getFunctionDefinitionParameters($phpcsFile, $functionNamePtr);
-		if (!$parameterTokens) {
-			return false;
-		}
-		foreach($parameterTokens as $index => $parameterToken) {
-			$variableName = $parameterToken['content'];
-			if (!in_array($variableName, $this->forbiddenParameterNames)) {
-				continue;
-			}
-			$phpcsFile->addWarning(sprintf('%s is not a valid function parameter', $variableName), $stackPtr);
-		}
-		return;
-	}
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $functionNamePtr = $phpcsFile->findNext(
+            array( T_STRING, ),
+            ($stackPtr + 1),
+            null, false
+        );
+
+        $parameterTokens = $this->getFunctionDefinitionParameters(
+            $phpcsFile, $functionNamePtr
+        );
+        if (!$parameterTokens) {
+            return false;
+        }
+        foreach($parameterTokens as $index => $parameterToken) {
+            $variableName = $parameterToken['content'];
+            if (!isset($this->forbiddenParameterNames[$variableName])) {
+                continue;
+            }
+            $phpcsFile->addWarning(
+                sprintf('%s is not a valid function parameter', $variableName),
+                $stackPtr
+            );
+        }
+    }
 }
