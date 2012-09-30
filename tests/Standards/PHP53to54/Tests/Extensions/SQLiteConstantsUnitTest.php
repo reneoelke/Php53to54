e57f450ac5e6ad64d50e8f880bcc246c2f3c339d
goatherd <maik@phpkuh.de>
2012-09-30 14:44:39 +0200

    Fixed tests, coding standard (PEAR) and documentation. Added issues for failed tests.

--- tests/Standards/PHP53to54/Tests/Extensions/SQLiteConstantsUnitTest.php
+++ tests/Standards/PHP53to54/Tests/Extensions/SQLiteConstantsUnitTest.php
@@ -5,29 +5,29 @@
  *
  * PHP version 5
  *
- * @category PHP
- * @package PHP_CodeSniffer
- * @author Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
  * @copyright 2012 foobugs oelke & eichner GbR
- * @license BSD http://www.opensource.org/licenses/bsd-license.php
- * @link https://github.com/foobugs/PHP53to54
- * @since 1.0-beta
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
  */
 
 /**
  * Unit test class for Extensions/SQLiteConstantSniff
  *
- * @group PHP53to54
- * @category PHP
- * @package PHP_CodeSniffer
- * @author Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @group     PHP53to54
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
  * @copyright 2012 foobugs oelke & eichner GbR
- * @license BSD http://www.opensource.org/licenses/bsd-license.php
- * @link https://github.com/foobugs/PHP53to54
- * @since 1.0-beta
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
  */
 class PHP53to54_Tests_PHP_SQLiteConstantsUnitTest
-extends PHP53to54_AbstractSniffUnitTest
+extends AbstractSniffUnitTest
 {
     /**
      * Returns the lines where errors should occur.
