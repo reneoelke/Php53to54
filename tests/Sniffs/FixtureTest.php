7b60b9b2343adeab4c528f91d6ffed7ca24dad9a
Maik Penz <maik@phpkuh.de>
2013-04-04 19:04:40 +0200

    find fixtures recurringly

--- tests/Sniffs/FixtureTest.php
+++ tests/Sniffs/FixtureTest.php
@@ -10,12 +10,15 @@ class FixtureTest extends \AbstractPhpcsTestCase
         // get fixture definitions from file
         $fixtures = array();
         $fixturePath = __DIR__ . '/_fixtures';
-        $it = new \FilesystemIterator($fixturePath);
+        $it = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($fixturePath),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
         foreach ($it as $fi) {
             $fn = substr($fi->getFilename(), -8);
             if ($fi->isFile() && $fn == '.def.php') {
                 $fixture = require($fi->getPathname());
-                $fixture[0] = $fixturePath . '/' . $fixture[0];
+                $fixture[0] = $fi->getPath() . '/' . $fixture[0];
                 $fixtures[] = $fixture;
             }
         }
