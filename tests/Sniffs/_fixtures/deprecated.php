df3ddfa141933bc06740019e4e6ba255a2742a86
Maik Penz <maik@phpkuh.de>
2013-04-04 22:41:19 +0200

    temporary codesniffer patch, fixed ruleset

---
+++ tests/Sniffs/_fixtures/deprecated.php
@@ -0,0 +1,10 @@
+<?php
+// function alias
+echo mysqli_client_encoding();
+
+var_dump('anything'.mysqli_get_metadata());
+
+// function
+import_request_variables();
+
+mcrypt_generic_end();
---
+++ tests/Sniffs/_fixtures/deprecatedFunctions.def.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    // fixture
+    'deprecated.php',
+    // standard
+    'php53to54',
+    // sniffs
+    array('PHP53to54.Sniffs.Deprecated.Functions'),
+    // errors
+    array(
+    ),
+    // warnings
+    array(
+    ),
+);
