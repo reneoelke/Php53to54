606aba11d21b58229bf1fc22c8bfda58315240c4
Maik Penz <maik@phpkuh.de>
2013-04-12 10:46:21 +0200

    migrated Functions and SNMP tests

---
+++ tests/Sniffs/SNMP/DeprecatedFunctionsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sniffs\Generic;
+
+/**
+ * @group DeprecatedFunctions
+ * @group SNMP
+ *
+ */
+class DeprecatedFunctionsTest extends \AbstractPhpcsTestCase
+{
+    protected $sniffs = array('PHP53to54.SNMP.DeprecatedFunctions');
+    protected $defaultType = "PHP53to54.SNMP.DeprecatedFunctions.Deprecated";
+
+    protected $warnings = array('21:1', '23:19', '25:9', '31:16');
+
+    /** {@inheritdoc} */
+    public function fixtureSniffProvider()
+    {
+        $this->fixture = __DIR__ . '/_fixtures/1.inc';
+        for ($i=36; $i < 60; $i++) {
+            $this->warnings[$i] = $i . ':1';
+        }
+        $fixtures = parent::fixtureSniffProvider();
+
+        return $fixtures;
+    }
+}
